8c1f5e13ac8c5fd42a00d067955b3276ee2d7f68
Ricardo Tonet <ribeiro.tonet@gmail.com>
2015-07-18 13:16:00 +0100

    Add base extension folders. Create configuration files and source options.

---
+++ app/code/community/BlackChacal/Prometheus/Model/Source/Config/Systemtabs.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * BlackChacal_Prometheus
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE_AFL.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Prometheus to newer
+ * versions in the future. If you wish to customize Prometheus for your
+ * needs please contact the author for more information.
+ *
+ * @category    BlackChacal
+ * @package     BlackChacal_Prometheus
+ * @copyright   Copyright (c) 2015 BlackChacal <ribeiro.tonet@gmail.com>
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+
+class BlackChacal_Prometheus_Model_Source_Config_Systemtabs
+{
+    /**
+     * Returns the configuration options for the
+     * "prometheus/default_extension/config_tab_system"
+     * - "Configuration Tab Name - System Name" select input.
+     *
+     * @access public
+     * @return array Configuration Options
+     */
+    public function toOptionArray()
+    {
+        $helper = Mage::helper('prometheus');
+
+        $options = array();
+        $options[] = array(
+            'value' => 'general',
+            'label' => $helper->__('General')
+        );
+        $options[] = array(
+            'value' => 'catalog',
+            'label' => $helper->__('Catalog')
+        );
+        $options[] = array(
+            'value' => 'customer',
+            'label' => $helper->__('Customers')
+        );
+        $options[] = array(
+            'value' => 'sales',
+            'label' => $helper->__('Sales')
+        );
+        $options[] = array(
+            'value' => 'service',
+            'label' => $helper->__('Services')
+        );
+        $options[] = array(
+            'value' => 'advanced',
+            'label' => $helper->__('Advanced')
+        );
+
+        return $options;
+    }
+}
---
+++ app/code/community/BlackChacal/Prometheus/Model/Source/Config/Tabtypes.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * BlackChacal_Prometheus
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE_AFL.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Prometheus to newer
+ * versions in the future. If you wish to customize Prometheus for your
+ * needs please contact the author for more information.
+ *
+ * @category    BlackChacal
+ * @package     BlackChacal_Prometheus
+ * @copyright   Copyright (c) 2015 BlackChacal <ribeiro.tonet@gmail.com>
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+
+class BlackChacal_Prometheus_Model_Source_Config_Tabtypes
+{
+    /**
+     * Returns the configuration options for the
+     * "prometheus/default_extension/config_tab" - "Configuration Tab Name"
+     * select input.
+     *
+     * @access public
+     * @return array Configuration Options
+     */
+    public function toOptionArray()
+    {
+        $helper = Mage::helper('prometheus');
+
+        $options = array();
+        $options[] = array(
+            'value' => 'namespace',
+            'label' => $helper->__('Namespace')
+        );
+        $options[] = array(
+            'value' => 'system',
+            'label' => $helper->__('System Tabs')
+        );
+        $options[] = array(
+            'value' => 'custom',
+            'label' => $helper->__('Custom')
+        );
+
+        return $options;
+    }
+}
