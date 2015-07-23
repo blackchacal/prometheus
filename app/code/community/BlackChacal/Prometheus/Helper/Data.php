a6dfbb7d25d4d14ab283c3c12403126771be9fad
Ricardo Tonet <ribeiro.tonet@gmail.com>
2015-07-23 08:21:29 +0100

    Create general helper functions: getConfig(), isPrometheusActive() and isHelpTabActive().

--- app/code/community/BlackChacal/Prometheus/Helper/Data.php
+++ app/code/community/BlackChacal/Prometheus/Helper/Data.php
@@ -23,5 +23,57 @@
 
 class BlackChacal_Prometheus_Helper_Data extends Mage_Core_Helper_Data
 {
+    /**
+     * System configuration section key
+     */
+    const XML_PATH_SECTION              = 'prometheus';
 
+    /**
+     * System configuration group keys
+     */
+    const XML_PATH_GENERAL_GROUP        = 'general';
+    const XML_PATH_DEF_EXTENSION_GROUP  = 'default_extension';
+
+    /**
+     * System configuration paths for "General" tab
+     */
+    const XML_PATH_GENERAL_ACTIVE       = 'prometheus/general/enabled';
+    const XML_PATH_GENERAL_HELP         = 'prometheus/general/help';
+
+
+    /**
+     * Returns the configuration options of Prometheus based on group and field.
+     *
+     * @param  string $group Configuration path - group
+     * @param  string $field Configuration path - field
+     * @param  mixed  $store Store data
+     * @return mixed
+     */
+    public function getConfig($group, $field, $store = null)
+    {
+        $configPath = self::XML_PATH_SECTION.'/'.$group.'/'.$field;
+        return Mage::getStoreConfig($configPath, $store);
+    }
+
+    /**
+     * Checks if the Prometheus extension is active or not. Returns true if active,
+     * false otherwise.
+     *
+     * @return boolean
+     */
+    public function isPrometheusActive()
+    {
+        return Mage::getStoreConfig(self::XML_PATH_GENERAL_ACTIVE);
+    }
+
+    /**
+     * Checks if the Help tab on the module edit page is active or not. Returns
+     * true if active, false otherwise.
+     *
+     * @return boolean
+     */
+    public function isHelpTabActive()
+    {
+        return Mage::getStoreConfig(self::XML_PATH_GENERAL_HELP);
+    }
 }
